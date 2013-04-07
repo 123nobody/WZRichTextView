f653b24def2e45f7061f895176bc04fdea8c34fb
123nobody <123nobody@gmail.com>
2013-04-07 10:36:35 +0800

    Initial Commit

---
+++ WZRichTextView/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  WZRichTextView
+//
+//  Created by Wei on 13-4-7.
+//  Copyright (c) 2013年 Wei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ WZRichTextView/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  WZRichTextView
+//
+//  Created by Wei on 13-4-7.
+//  Copyright (c) 2013年 Wei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
